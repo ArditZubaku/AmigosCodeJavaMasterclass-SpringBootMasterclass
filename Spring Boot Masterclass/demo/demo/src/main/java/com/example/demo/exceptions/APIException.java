f2f274aff2a58c481f6d72b212da4fbb81c7ba4a
zubaku92 <zubaku92@gmail.com>
2023-03-22 15:26:53 +0100

    Using records when possible

--- Spring Boot Masterclass/demo/demo/src/main/java/com/example/demo/exceptions/APIException.java
+++ Spring Boot Masterclass/demo/demo/src/main/java/com/example/demo/exceptions/APIException.java
@@ -5,12 +5,15 @@ import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
-@Data // equivalent to @Getter, @Setter, @RequiredArgsConstructor, @ToString, @EqualsAndHashCode
-public class APIException {
-    private final String message;
+//@Data // equivalent to @Getter, @Setter, @RequiredArgsConstructor, @ToString, @EqualsAndHashCode
+public record APIException(String message, Throwable throwable, HttpStatus httpStatus, ZonedDateTime zonedDateTime) {
+    // private final, getter, public constructor, equals, hashcode, toString
+    // the purpose of records is to achieve immutability
+
+   /* private final String message;
     private final Throwable throwable;
     private final HttpStatus httpStatus;
-    private final ZonedDateTime zonedDateTime;
+    private final ZonedDateTime zonedDateTime;*/
 
 /*    public APIException(String message,
                         Throwable throwable,
