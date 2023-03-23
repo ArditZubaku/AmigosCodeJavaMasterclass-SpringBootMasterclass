1da1c9abe63c561f19d62a70e23c941ba9d26bc2
zubaku92 <zubaku92@gmail.com>
2023-03-23 09:42:30 +0100

    Simple API

---
+++ Spring Security/demo/demo/src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,5 @@
+package com.example.demo.student;
+
+public record Student(Integer studentId, String studentName) {
+
+}
---
+++ Spring Security/demo/demo/src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,31 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENT_LIST = Arrays.asList(
+            new Student(1, "Student1"),
+            new Student(2, "Student2"),
+            new Student(3, "Student3")
+    );
+
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+
+        return STUDENT_LIST.stream()
+                .filter(student -> studentId.equals(student.studentId()))
+                .findFirst()
+                 .orElseThrow(() -> new IllegalStateException("Student " + studentId + " does not exist!"));
+
+    }
+}
