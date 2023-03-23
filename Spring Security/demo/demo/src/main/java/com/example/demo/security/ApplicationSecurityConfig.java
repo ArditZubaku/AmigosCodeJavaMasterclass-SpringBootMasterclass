1b3a5790a100c43db7a371651414d295b46ae3f3
zubaku92 <zubaku92@gmail.com>
2023-03-23 11:09:39 +0100

    White-listing URLs

---
+++ Spring Security/demo/demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // authorize requests
+        // any request must be authenticated, i.e. the client must specify the username and password
+        // the mechanism to enforce the authenticity of a client -> basic auth
+        http    .authorizeRequests()
+                .antMatchers("/","index","/css/*","/js/*")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
