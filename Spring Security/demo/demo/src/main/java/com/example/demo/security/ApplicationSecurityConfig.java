7555ea8df2a4ea1be2efc3bf55eded1693b9d4ef
zubaku92 <zubaku92@gmail.com>
2023-03-23 11:55:09 +0100

    PasswordEncoder, roles, permissions and role based auth

--- Spring Security/demo/demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ Spring Security/demo/demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,24 +1,59 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.example.demo.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // authorize requests
         // any request must be authenticated, i.e. the client must specify the username and password
         // the mechanism to enforce the authenticity of a client -> basic auth
-        http    .authorizeRequests()
-                .antMatchers("/","index","/css/*","/js/*")
-                .permitAll()
+        http.authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails user1 = User.builder()
+                .username("user1")
+                .password(passwordEncoder.encode("password1"))
+                .roles(STUDENT.name()) // internally: ROLE_STUDENT
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("password2"))
+                .roles(ADMIN.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(user1,admin);
     }
 }
